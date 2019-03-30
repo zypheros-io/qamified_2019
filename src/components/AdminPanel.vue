0ab636dc93d4ef4b25dfeb6f3b614e313f0c3c61
Kenichii <ana.kenichii@gmail.com>
2019-03-30 22:53:05 +0800

    Added logs view on admin panel

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -10,6 +10,21 @@
       >
         <b-tab-item label="Dashboard"></b-tab-item>
         <b-tab-item label="Logs">
+          <b-field label="Filter by:" class="is-pulled-right">
+            <b-select v-model="filterKey">
+              <option value="ALL">ALL</option>
+              <option value="POST_QUEST">POST QUEST</option>
+              <option value="POST_SOLUTION">POST SOLUTION</option>
+              <option value="POST_REPLY">POST REPLY</option>
+              <option value="DELETE_QUEST">DELETE QUEST</option>
+              <option value="DELETE_SOLUTION">DELETE SOLUTION</option>
+              <option value="DELETE_REPLY">DELETE REPLY</option>
+              <option value="UPVOTE_QUEST">UPVOTE_QUEST</option>
+              <option value="UPVOTE_SOLUTION">UPVOTE_SOLUTION</option>
+              <option value="DOWNVOTE_QUEST">DOWNVOTE_QUEST</option>
+              <option value="DOWNVOTE_SOLUTION">DOWNVOTE_SOLUTION</option>
+            </b-select>
+          </b-field>
           <table
             class="table container is-fullwidth is-striped is-hoverable is-bordered is-scrollable"
             id="logs-table"
@@ -18,9 +33,22 @@
               <tr>
                 <th>Timestamp</th>
                 <th>User</th>
+                <th>Username</th>
                 <th>Activity</th>
               </tr>
             </thead>
+            <tbody>
+              <tr
+                v-if="filterLogs(log)"
+                v-for="(log, index) in logs"
+                :key="index"
+              >
+                <td>{{ log.date_created }}</td>
+                <td>{{ log.fullname }}</td>
+                <td>{{ log.username }}</td>
+                <td>{{ log.context }}</td>
+              </tr>
+            </tbody>
           </table>
         </b-tab-item>
         <b-tab-item label="Users">
@@ -73,12 +101,16 @@ export default {
   data() {
     return {
       username: 'Kenichii',
-      activeTab: 2
+      activeTab: 2,
+      filterKey: 'ALL'
     };
   },
   computed: {
     users() {
       return this.$store.getters['admin/users'];
+    },
+    logs() {
+      return this.$store.getters['admin/logs'];
     }
   },
   methods: {
@@ -95,6 +127,14 @@ export default {
         hasIcon: true,
         onConfirm: () => this.$store.dispatch('admin/banUser', user)
       });
+    },
+    filterLogs: function filterLogs(log) {
+      // Filter results
+      if (this.filterKey === 'ALL' || log.context === this.filterKey) {
+        return true;
+      }
+      // If doesn't match filter
+      return false;
     }
   },
   mounted() {
@@ -107,7 +147,7 @@ export default {
 #admin-panel {
   height: 100vh;
   width: 100%;
-  padding-top: 100px;
+  padding: 100px 15px 15px 15px;
 }
 #admin-panel .level,
 #admin-panel .level-item {
